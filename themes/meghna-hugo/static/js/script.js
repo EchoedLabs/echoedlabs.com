fc315f3cff0229848a815f214d006ffc8f6bb60a
xmodulus <me@ziz.ninja>
2019-01-07 17:24:55 +0800

    fixed forms

--- themes/meghna-hugo/static/js/script.js
+++ themes/meghna-hugo/static/js/script.js
@@ -144,10 +144,15 @@ jQuery(function ($) {
 				'disabled': 'false',
 				'value': 'Sending...'
 			});
-
+			//if the mail is sent remove the submit paragraph
+			$('#cf-submit').remove();
+			//and show the mail success div with fadeIn
+			$('#mail-success').fadeIn(500);
+			$('#contact-submit').removeAttr('disabled').attr('value', 'Send The Message');
 			/* using the jquery's post(ajax) function and a lifesaver
 			function serialize() which gets all the data from the form
 			we submit it to send_email.php */
+			/*
 			$.post("sendmail.php", $("#contact-form").serialize(), function (result) {
 				//and after the ajax request ends we check the text returned
 				if (result == 'sent') {
@@ -162,6 +167,7 @@ jQuery(function ($) {
 					$('#contact-submit').removeAttr('disabled').attr('value', 'Send The Message');
 				}
 			});
+			*/
 		}
 	});
 
